85cbbced6f129fffb268d7f3581661b6afb5fdc5
walidbbq <ghanim.w@gmail.com>
2018-12-20 15:42:45 +0100

    update

---
+++ Test/src/ObserverTest.java
@@ -0,0 +1,76 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ObserverTest {
+
+    public static void main(String[] args) {
+        // ====
+        LichtschrankeP ls = new LichtschrankeP();
+        ls.id = 124;
+        AutomatischeTuerP at = new AutomatischeTuerP(ls);
+        ls.unterbrechen();
+    }
+}
+// Observer Pattern wird durch Interfaces umgesetzt: 
+
+interface Beobachtbar {
+    void registriereBeobachter(Beobachter b);
+    void deregistriereBeobachter(Beobachter b);
+    void benachrichtigeBeobachter();
+}
+
+interface Beobachter {
+    void aktualisiere(int id);
+}
+
+class LichtschrankeP implements Beobachtbar {
+    int id;
+
+
+    private List<Beobachter> liste = new ArrayList<>();
+
+    void unterbrechen() {
+        System.out.println("Lichtschranke wird unterbrochen");
+        benachrichtigeBeobachter();
+    }
+    @Override
+    public void registriereBeobachter(Beobachter b) {
+        liste.add(b);
+    }
+    @Override
+    public void deregistriereBeobachter(Beobachter b) {
+        liste.remove(b);
+    }
+    @Override
+    public void benachrichtigeBeobachter() {
+        for (Beobachter b : liste) {
+            b.aktualisiere(this.id);
+        }
+
+    }
+}
+
+
+class AutomatischeTuerP implements Beobachter {
+
+    // AutomatischeTuer muss sich bei Lichtschranke registrieren
+    AutomatischeTuerP(Beobachtbar bb) {
+//        bb.deregistriereBeobachter(this);
+        bb.registriereBeobachter(this);
+    }
+
+    void oeffnen() {
+        System.out.println("Tür öffnet sich");
+    }
+
+    @Override
+    public void aktualisiere(int id) {
+        System.out.println("Aktualisiere kommt von id=" + id);
+        if (id==124)
+            this.oeffnen();	// Hier steht die Aktion, die ausgelöst wird,
+        // wenn die Lichtschranke unterbrochen wird.
+    }
+
+}
